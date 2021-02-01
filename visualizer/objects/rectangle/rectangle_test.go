ef5c3ed6230c077a0c4ede78f7e5b24a02895e76
Henrique <henrique.brunialti@gmail.com>
2021-02-01 09:06:43 -0300

    Test rectangle HasHitScreenBorder

--- visualizer/objects/rectangle/rectangle_test.go
+++ visualizer/objects/rectangle/rectangle_test.go
@@ -3,6 +3,7 @@ package rectangle
 import ( 
    "testing"
    "al.go/terminal"
+   "al.go/testing/mocks"
 )
 
 func TestMove(t *testing.T) {
@@ -12,7 +13,7 @@ func TestMove(t *testing.T) {
 	destinationPoint := terminal.Point{X: originalPoint.X + velX, Y: originalPoint.Y + velY,}
 	rect.Move(velX, velY)
 	if rect.TopLeftCorner != destinationPoint {
-		t.Errorf("Unexpected destination point after moving rectangle: expected %v, got: %v", destinationPoint, rect.TopLeftCorner)
+		t.Errorf("Unexpected destination point after moving the rectangle: expected %v, got: %v", destinationPoint, rect.TopLeftCorner)
 	}
 
 	if rect.Height != 10 {
@@ -25,7 +26,52 @@ func TestMove(t *testing.T) {
 }
 
 func TestHasHitScreenBorder(t *testing.T) {
+	scr := &mocks.ScreenMock{
+		SizeFunc: func() (int, int) {
+			return 50, 50
+		},
+	}
+
+	w, h := 5, 5
+
+	points := []terminal.Point{ 
+								{X: 10, Y: 10}, 
+								{X: 0, Y: 10},
+								{X: 45, Y:10},
+								{X: 10, Y: 0},
+								{X: 10, Y: 45},
+							  }
 	
+	expectedResults := []struct{
+			any bool
+			top bool
+			bottom bool
+			left bool
+			right bool
+	}{
+		{ any: false, top: false, bottom: false, left: false, right: false },
+		{ any: true, top: false, bottom: false, left: true, right: false },
+		{ any: true, top: false, bottom: false, left: false, right: true },
+		{ any: true, top: true, bottom: false, left: false, right: false },
+		{ any: true, top: false, bottom: true, left: false, right: false },
+	}
 
+	for i, p := range points {
+		rect := Rectangle{TopLeftCorner: p, Width: w, Height: h, }
+		expectedResult := expectedResults[i]
+		
+		any, top, bottom, left, right := rect.HasHitScreenBorder(scr)
 
+		if expectedResult.any == any &&
+		   expectedResult.top == top &&
+		   expectedResult.bottom == bottom &&
+		   expectedResult.left == left &&
+		   expectedResult.right == right {
+			   continue
+		   } else {
+			   t.Errorf("Invalid result for HasHitScreenBorder at point: %v \n(any, top, bottom, left, right)\nexpected:\n%v, %v, %v, %v, %v\ngot:\n%v, %v, %v, %v", p,
+			   any, top, bottom, left, right, 
+			   expectedResult.any, expectedResult.bottom, expectedResult.left, expectedResult.right)
+		   }
+	}
 }
